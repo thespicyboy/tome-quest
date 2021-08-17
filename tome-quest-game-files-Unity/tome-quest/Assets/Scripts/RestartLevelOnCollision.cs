386ef1ff4dbf626c4fc952cac44db1ebe4134ff1
Colin Farrell <colinfarrell@protonmail.com>
2021-08-17 14:38:21 -0400

    prototype attempt
    first room, camera, and player with controls. press R to restart scene.

---
+++ tome-quest-game-files-Unity/tome-quest/Assets/Scripts/RestartLevelOnCollision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    string strTag;
+
+    // Restart Scene on Collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == strTag)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}

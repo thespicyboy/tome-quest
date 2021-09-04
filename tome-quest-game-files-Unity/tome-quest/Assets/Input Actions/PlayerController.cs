12a59147ff0f7739d475a41021296ac33b231414
thespicyboy <58871947+thespicyboy@users.noreply.github.com>
2021-09-04 17:22:25 -0400

    added camera functions and better input controls
    Cinemachine package for lots of camera utility
    
    better Input System controls for M/KB and gamepad

--- tome-quest-game-files-Unity/tome-quest/Assets/Input Actions/PlayerController.cs
+++ tome-quest-game-files-Unity/tome-quest/Assets/Input Actions/PlayerController.cs
@@ -1,44 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//automatically add missing component to parent object, in this case a CharacterController
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    private PlayerInputActions playerInputActions;
-    private InputAction movement;
+    [SerializeField]
+    private InputActionReference movementControl;
+    [SerializeField]
+    private InputActionReference jumpControl;
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float rotationSpeed = 4.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private Transform cameraMainTrans;
 
-    private void Awake()
-    {
-        playerInputActions = new PlayerInputActions();
-    }
-
+    //enable input actions?? when parent object is enabled maybe?
     private void OnEnable()
     {
-        movement = playerInputActions.Player.Movement;
-        movement.Enable();
-
-        playerInputActions.Player.Jump.performed += DoJump;
-        playerInputActions.Player.Jump.Enable();
-
+        movementControl.action.Enable();
+        jumpControl.action.Enable();
     }
-
-    private void DoJump(InputAction.CallbackContext obj)
-    {
-        Debug.Log("Jump!");
-    }
-
+    //disable input actions?? when parent object is disabled maybe?
     private void OnDisable()
     {
-        movement.Disable();
-        playerInputActions.Player.Jump.Disable();
+        movementControl.action.Disable();
+        jumpControl.action.Disable();
     }
 
-    private void FixedUpdate()
+
+    private void Start()
     {
-        Debug.Log("Movement Values " + movement.ReadValue<Vector2>());
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraMainTrans = Camera.main.transform;
     }
 
+    void Update()
+    {
+        //keep player on ground
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        //read movement input vector from mouse or gamepad joystick
+        Vector2 movement = movementControl.action.ReadValue<Vector2>();
+        Debug.Log("Movement Values " + movement);
+
+        //assign 2d movement input to 3d movement vector target
+        Vector3 moveTarget = new Vector3(movement.x, 0f, movement.y);
+        moveTarget = cameraMainTrans.forward * moveTarget.z + cameraMainTrans.right * moveTarget.x;
+        moveTarget.y = 0f;
+        controller.Move(moveTarget * Time.deltaTime * playerSpeed); //apply movement velocity
+        
+
+        // Changes the height position of the player..
+        if (jumpControl.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            Debug.Log("Jump!");
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
 
 
+        //rotate player towards input rotation, considering camera position
+        if (movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTrans.eulerAngles.y;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
 }
--- tome-quest-game-files-Unity/tome-quest/Assets/Input Actions/PlayerInputActions.cs
+++ tome-quest-game-files-Unity/tome-quest/Assets/Input Actions/PlayerInputActions.cs
@@ -33,6 +33,14 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""55a60cc6-ea63-4e1a-b92c-c818b54c50be"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -178,6 +186,28 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                     ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3fd5e7bb-a566-4d16-9175-f5aa87cc1f4f"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f8c96839-d588-4b9a-bf85-bcbb83bbc836"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -199,6 +229,7 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -250,12 +281,14 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Movement;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Look;
     public struct PlayerActions
     {
         private @PlayerInputActions m_Wrapper;
         public PlayerActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Movement => m_Wrapper.m_Player_Movement;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -271,6 +304,9 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Look.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -281,6 +317,9 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
             }
         }
     }
@@ -307,5 +346,6 @@ public class @PlayerInputActions : IInputActionCollection, IDisposable
     {
         void OnMovement(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
     }
 }
